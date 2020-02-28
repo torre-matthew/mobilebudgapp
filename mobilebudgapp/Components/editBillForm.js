8c3070db1795fea97a1366c62a90b5a199b67bff
Torre Matthew <torre.matthew@gmail.com>
2020-02-27 22:02:06 -0500

    added dropdown for selecting funding source on unplanned bills

--- mobilebudgapp/Components/editBillForm.js
+++ mobilebudgapp/Components/editBillForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Dimensions, TouchableOpacity } from 'react-native';
+import { View, Dimensions, TouchableOpacity, Picker } from 'react-native';
 import { Container, Content, Form, Item, Input, Text } from 'native-base';
+import FundingSourceDisplay from "./fundingSourceDisplay";
 import style from "../Styles/Styles";
 
 function EditBillFormDisplay (props) {
@@ -16,6 +17,23 @@ function EditBillFormDisplay (props) {
                         </Item>
                         <Item>
                             <Input defaultValue={props.billAmount} keyboardType='numeric' placeholder="Amount" onChangeText={props.handleBillAmount} />
+                        </Item>
+                        <Item>
+                        <Picker
+                        prompt="Select Funding Source"
+                        selectedValue={this.state.fundingSource}
+                        style={{height: 50, width: 500}}
+                        onValueChange={(itemValue, itemIndex) =>
+                            this.setState({fundingSource: itemValue})
+                        }>
+                            {props.incomeDataFromDB.map(income => 
+                                <Picker.Item 
+                                    label={income.name + " - " + "$" + income.amount + " on " + income.date} 
+                                    value={income.name} 
+                                    key={income.name + "-" + income.amount + "-" + Math.floor((Math.random() * 100000) + 1)}
+                                    />
+                                )}                        
+                        </Picker>
                         </Item>
                         <View style={{ alignItems: 'center' }}>
                         <TouchableOpacity
