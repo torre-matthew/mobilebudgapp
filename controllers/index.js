fead6ce97b6d8755defff5b5c959d405ed12abfa
Torre Matthew <torre.matthew@gmail.com>
2020-03-28 14:40:08 -0400

    updates

--- controllers/index.js
+++ controllers/index.js
@@ -104,22 +104,21 @@ let editIncomeByID = (req, res) => {
 let getUpdatedCheckAfterSpendingAmount = (req, res) => {
     let totalOfExpenses = 0;
     let availableIncomeAmount = 0;
-    console.log(req.params.incomeID);
-    
-    // db.Expenses.find({fundingSource: req.params.incomeID})
-    // .then(data => {
-    //     data.forEach(element => {
-    //         totalOfExpenses += parseFloat(element.amountOfExpense);
-    //     });
-    //         db.Income.find({_id: req.params.incomeID})
-    //         .then(data => {
-    //                 availableIncomeAmount = parseFloat(data[0].amount) - totalOfExpenses;
-    //                 console.log(availableIncomeAmount);
-    //                 return availableIncomeAmount;
-    //             })
-    //         .catch(err => console.log(err));
-    //     })
-    // .catch(err => console.log(err));
+    console.log(req.params.incomeID);    
+    db.Expenses.find({fundingSource: req.params.incomeID})
+    .then(data => {
+        data.forEach(element => {
+            totalOfExpenses += parseFloat(element.amountOfExpense);
+        });
+            db.Income.find({_id: req.params.incomeID})
+            .then(data => {
+                    availableIncomeAmount = parseFloat(data[0].amount) - totalOfExpenses;
+                    console.log(availableIncomeAmount);
+                    return availableIncomeAmount;
+                })
+            .catch(err => console.log(err));
+        })
+    .catch(err => console.log(err));
 }
 
 module.exports = {
