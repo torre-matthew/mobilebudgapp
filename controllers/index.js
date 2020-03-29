d15aca63915a258f9596a817535c7a2cf63ac263
Torre Matthew <torre.matthew@gmail.com>
2020-03-28 23:22:08 -0400

    updates

--- controllers/index.js
+++ controllers/index.js
@@ -107,7 +107,7 @@ let updateAfterSpendingAmount = (req, res) => {
     let availableIncomeAmount = 0;
     db.Expenses.find({fundingSource: req.params.incomeID})
     .then(data => {
-        console.log("finding expense data" + data === null);
+        console.log(data);
 
         if (data === []) {
             totalOfExpenses = 0;    
