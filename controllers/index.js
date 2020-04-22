73275ef69212db27ac9f568cefca598f13edefdd
Torre Matthew <torre.matthew@gmail.com>
2020-04-21 23:35:16 -0400

    updates

--- controllers/index.js
+++ controllers/index.js
@@ -170,17 +170,17 @@ let editIncomeByID = (req, res) => {
     .catch(err => console.log(err));
 }
 
-let updateIncomeOnUserRecord = (req, res) => {
+let updateIncomeOnUserRecord = (userID) => {
     //first, empty the the income array for the user
     db.Users
-    .updateOne({_id: req.params.userID}, { $set: { income: [] } }, { new: true })
+    .updateOne({_id: userID}, { $set: { income: [] } }, { new: true })
         .then(data => {
     //then find all income with that userID
-                db.Income.find({userID: req.params.userID}) 
+                db.Income.find({userID: userID}) 
                 .then(userIncomeArrayFromDB => {
     //then repopulate the income array on the user with the latest 
                     userIncomeArrayFromDB.forEach(userIncomeRecordObject => {
-                            db.Users.updateOne({_id: req.params.userID}, { $push: { income: userIncomeRecordObject._id } }, { new: true })
+                            db.Users.updateOne({_id: userID}, { $push: { income: userIncomeRecordObject._id } }, { new: true })
                             .then(data => res.json(data))
                             .catch(err => console.log(err))
                         });
