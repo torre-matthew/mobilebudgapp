9f6b404abf6dc4cb93573993603e0b47234dea4c
Torre Matthew <torre.matthew@gmail.com>
2022-03-05 17:01:05 -0500

    updates

--- server.js
+++ server.js
@@ -39,9 +39,9 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Access-Control-Allow-Headers, Content-Type, Authorization, Origin, Accept");
   res.setHeader('Access-Control-Allow-Credentials', true)
-  // res.append('Access-Control-Allow-Origin', '*');
-  // res.append('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  // res.append('Access-Control-Allow-Headers', 'Content-Type');
+  res.append('Access-Control-Allow-Origin', '*');
+  res.append('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
