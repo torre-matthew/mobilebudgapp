0bf8b2014a08c810296aabf74b9099478d40ded6
Torre Matthew <torre.matthew@gmail.com>
2022-03-05 15:30:19 -0500

    updating server.js file

---
+++ db/models/expense.js
@@ -0,0 +1,56 @@
+let mongoose = require("mongoose");
+
+let Schema = mongoose.Schema;
+
+let ExpenseSchema = new Schema({
+nameOfExpense: {
+      type: String,
+      required: true
+  },
+dateOfExpense: {
+      type: Date,
+      default: Date.now,
+      required: true
+  },
+amountOfExpense: {
+    type: String,
+    required: true
+},
+isPlanned: {
+    type: Boolean,
+    required: true
+},
+isPaid: {
+  type: Boolean,
+  required: true
+},
+forBillTracker: {
+  type: Boolean,
+  required: false
+},
+fundingSource: {
+    type: String,
+    required: false
+  },
+userID: {
+    type: String,
+    required: true
+  },
+monthID: {
+    type: String,
+    required: true
+  },
+categoryName: {
+    type: String,
+    required: false
+  },
+categoryID: {
+  type: String,
+  required: false
+}
+});
+
+// This creates our model from the above schema, using mongoose's model method
+let ExpenseData = mongoose.model("Expenses", ExpenseSchema);
+
+module.exports = ExpenseData;
