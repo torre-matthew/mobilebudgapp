004fa8da45a83618aebe0b177589f0bd7bcf1009
Torre Matthew <torre.matthew@gmail.com>
2022-03-06 10:58:15 -0500

    updates

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const axios = require("axios");
 var cors = require('cors')
 const app = express();
+app.use(cors());
 // const port = 3001;
 const PORT = process.env.PORT || 3001;
 const routes = require("./routes");
